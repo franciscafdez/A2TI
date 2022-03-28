479ad02778accf9a2090bd2eaecb84b02257ec21
franciscafdez <fafernandez2@uc.cl>
2022-03-28 16:16:01 -0300

    https protocol in url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/info', (req,res)=>{
-    res.status(200).send({"url": req.protocol + '://' + req.get('host')})
+    res.status(200).send({"url": req.protocol +'s'+ '://' + req.get('host')})
 })
 
 app.get('/status', (req,res)=>{
