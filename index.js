4f3f8e92fec582178f5cd87a25df33497a0397b2
franciscafdez <fafernandez2@uc.cl>
2022-03-28 09:41:13 -0300

    first api commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
+const app = express()
+
+
+const port = 3000
+
+app.get('/info', (req,res)=>{
+    res.status(200).send({"url": req.url})
+})
+
+app.get('/status', (req,res)=>{
+    res.status(204).send("204 No Content")
+})
+
+app.delete('/security',(req,res)=>{
+    res.status(401).send('401 Unauthorized')
+})
+
+app.listen(port,()=>{
+    console.log("App is listening port"+ port)
+})
