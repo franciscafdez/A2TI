13ea1d9a2f05c63baef3ae3bf601180f1139c759
franciscafdez <fafernandez2@uc.cl>
2022-03-28 10:03:42 -0300

    url fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/info', (req,res)=>{
-    res.status(200).send({"url": req.url})
+    res.status(200).send({"url": req.protocol + '://' + req.get('host') +req.url})
 })
 
 app.get('/status', (req,res)=>{
